2ad10cd008356335b6448b3f874f179e97a07239
Vanik Hakobyan <van19962013@mail.ru>
2017-05-05 12:12:22 +0400

    :+1: add correctly input mode :1:

--- ProcessOS/ProcessGROUP/First.cpp
+++ ProcessOS/ProcessGROUP/First.cpp
@@ -7,15 +7,15 @@ void main()
 {
 	STARTUPINFO x;
 	PROCESS_INFORMATION y;
-	char cmd[300] = "c:\\path\\second.exe ";
+	char cmd[300] = "C:\\Users\\Student\\Documents\\Visual Studio 2015\\second\\debug\\second.exe ";
 	ZeroMemory(&x, sizeof(x));
 	x.cb = sizeof(x);
 	int pid = GetCurrentProcessId();
 	cout << "current process ID=" << pid << endl;;
 	char a[15];
-	itoa(pid, a, 10);
+	_itoa_s(pid, a, 15, 10);
 	//cout<<endl<<"a="<<a<<endl;
-	strcat(cmd, a); cout << cmd << endl;
+	strcat_s(cmd, a); cout << cmd << endl;
 	WCHAR cmdw[300];
 	for (int i = 0; i<300; i++) cmdw[i] = (WCHAR)cmd[i];
 	cout << "first started\n";
