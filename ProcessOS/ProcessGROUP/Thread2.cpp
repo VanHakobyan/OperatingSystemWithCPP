e9dea72767fb048a24b43265b325452c71869eaa
VanHakobyan <van19962013@mail.ru>
2017-05-08 22:27:56 +0400

    add Thread2 :+1:

---
+++ ProcessOS/ProcessGROUP/Thread2.cpp
@@ -0,0 +1,30 @@
+#include <iostream.h>
+#include <windows.h>
+#include <process.h>
+
+void thread1(void *)
+{
+	int i = 0;
+	while (1) {
+		cout << i << "\t"; i += 2;
+		Sleep(5);
+	}
+}
+
+void thread2(void *)
+{
+	int i = 1;
+	while (1) {
+		cout << i << "\t"; i += 2;
+		Sleep(5);
+	}
+}
+
+void main()
+{
+	cout << "main started\n";
+	_beginthread(thread1, 0, NULL);
+	_beginthread(thread2, 0, NULL);
+	Sleep(5000);
+	cout << "\n main finished\n";
+}
